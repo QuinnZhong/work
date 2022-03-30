b33b3bab73dae8ea07fbdc7e64a59191c2a05e51
Quinn <90532540+QuinnZhong@users.noreply.github.com>
2022-03-30 04:34:31 -0400

    web (1st update)

---
+++ test-it/js/custom.js
@@ -0,0 +1,114 @@
+(function($) {
+    "use strict";
+	
+	/* ..............................................
+	   Loader 
+	   ................................................. */
+	$(window).on('load', function() {
+		$('.preloader').fadeOut();
+		$('#preloader').delay(550).fadeOut('slow');
+		$('body').delay(450).css({
+			'overflow': 'visible'
+		});
+	});
+
+	/* ..............................................
+	   Gallery
+	   ................................................. */
+
+	$('#slides-shop').superslides({
+		inherit_width_from: '.cover-slides',
+		inherit_height_from: '.cover-slides',
+		play: 5000,
+		animation: 'fade',
+	});
+
+	$(".cover-slides ul li").append("<div class='overlay-background'></div>");
+
+	/* ..............................................
+	   Special Menu
+	   ................................................. */
+
+	var Container = $('.container');
+	Container.imagesLoaded(function() {
+		var portfolio = $('.special-menu');
+		portfolio.on('click', 'button', function() {
+			$(this).addClass('active').siblings().removeClass('active');
+			var filterValue = $(this).attr('data-filter');
+			$grid.isotope({
+				filter: filterValue
+			});
+		});
+		var $grid = $('.special-list').isotope({
+			itemSelector: '.special-grid'
+		});
+	});
+
+	/* ..............................................
+	   Tooltip
+	   ................................................. */
+
+	$(document).ready(function() {
+		$('[data-toggle="tooltip"]').tooltip();
+	});
+
+	/* ..............................................
+	   Owl Carousel Instagram Feed
+	   ................................................. */
+
+	$('.main-instagram').owlCarousel({
+		loop: true,
+		margin: 0,
+		dots: false,
+		autoplay: true,
+		autoplayTimeout: 3000,
+		autoplayHoverPause: true,
+		navText: ["<i class='fas fa-arrow-left'></i>", "<i class='fas fa-arrow-right'></i>"],
+		responsive: {
+			0: {
+				items: 2,
+				nav: true
+			},
+			600: {
+				items: 3,
+				nav: true
+			},
+			1000: {
+				items: 5,
+				nav: true,
+				loop: true
+			}
+		}
+	});
+
+	/* ..............................................
+	   Featured Products
+	   ................................................. */
+
+	$('.featured-products-box').owlCarousel({
+		loop: true,
+		margin: 15,
+		dots: false,
+		autoplay: true,
+		autoplayTimeout: 3000,
+		autoplayHoverPause: true,
+		navText: ["<i class='fas fa-arrow-left'></i>", "<i class='fas fa-arrow-right'></i>"],
+		responsive: {
+			0: {
+				items: 1,
+				nav: true
+			},
+			600: {
+				items: 3,
+				nav: true
+			},
+			1000: {
+				items: 4,
+				nav: true,
+				loop: true
+			}
+		}
+	});
+
+		
+}(jQuery));
